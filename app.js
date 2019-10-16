d94f7bd6e56d710bd7663857247ccf5fe9a75e60
abirhasanmubin <abirhasanmubin@gmail.com>
2019-10-17 01:54:45 +0600

    added compression and helmet @ app

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression')
+var helmet = require('helmet')
 require('dotenv').config()
 
 
@@ -11,8 +13,12 @@ var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
 var app = express();
+
+app.use(helmet())
+
 var mongoose = require('mongoose');
 var mongoDB = process.env.library_db;
+// var mongoDB = process.env.MONGODB_URI
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -25,6 +31,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression())
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
